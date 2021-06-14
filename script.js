a94066f3bc2a8d448cfb2347f563ea91fcc50b50
fpfc <lore_herrera95@hotmail.com>
2021-06-14 14:43:48 -0500

    “ipa”

---
+++ script.js
@@ -0,0 +1,243 @@
+document.getElementById("eat").addEventListener("click", ()=>{
+    document.getElementById("sound-eat").play();
+})
+
+document.getElementById("it").addEventListener("click", ()=>{
+    const sound_it = document.getElementById("sound-it").play();
+})
+
+const book = document.getElementById("book")
+book.addEventListener("click", ()=>{
+    const sound_book = document.getElementById("sound-book")
+    sound_book.play();
+})
+
+const blue = document.getElementById("blue")
+blue.addEventListener("click", ()=>{
+    const sound_blue = document.getElementById("sound-blue")
+    sound_blue.play();
+})
+
+const bed = document.getElementById("bed")
+bed.addEventListener("click", ()=>{
+    const sound_bed = document.getElementById("sound-bed")
+    sound_bed.play();
+})
+
+const schwa = document.getElementById("schwa")
+schwa.addEventListener("click", ()=>{
+    const sound_schwa = document.getElementById("sound-schwa")
+    sound_schwa.play();
+})
+
+const first = document.getElementById("first")
+first.addEventListener("click", ()=>{
+    const sound_first = document.getElementById("sound-first")
+    sound_first.play();
+})
+
+const on = document.getElementById("on")
+on.addEventListener("click", ()=>{
+    const sound_on = document.getElementById("sound-on")
+    sound_on.play();
+})
+
+const cat = document.getElementById("cat")
+cat.addEventListener("click", ()=>{
+    const sound_cat = document.getElementById("sound-cat")
+    sound_cat.play();
+})
+
+const us = document.getElementById("us")
+us.addEventListener("click", ()=>{
+    const sound_us = document.getElementById("sound-us")
+    sound_us.play();
+})
+
+const got = document.getElementById("got")
+got.addEventListener("click", ()=>{
+    const sound_got = document.getElementById("sound-got")
+    sound_got.play();
+})
+
+/* const british = document.getElementById("british")
+british.addEventListener("click", ()=>{
+    const sound_british = document.getElementById("sound-british")
+    sound_british.play();
+}) */
+
+const make = document.getElementById("make")
+make.addEventListener("click", ()=>{
+    const sound_make = document.getElementById("sound-make")
+    sound_make.play();
+})
+
+const boy = document.getElementById("boy")
+boy.addEventListener("click", ()=>{
+    const sound_boy = document.getElementById("sound-boy")
+    sound_boy.play();
+})
+
+const like = document.getElementById("like")
+like.addEventListener("click", ()=>{
+    const sound_like = document.getElementById("sound-like")
+    sound_like.play();
+})
+
+const cloud = document.getElementById("cloud")
+cloud.addEventListener("click", ()=>{
+    const sound_cloud = document.getElementById("sound-cloud")
+    sound_cloud.play();
+})
+
+const pai = document.getElementById("pai")
+pai.addEventListener("click", ()=>{
+    const sound_pai = document.getElementById("sound-pai")
+    sound_pai.play();
+})
+
+const bob = document.getElementById("bob")
+bob.addEventListener("click", ()=>{
+    const sound_bob = document.getElementById("sound-bob")
+    sound_bob.play();
+})
+
+const toy = document.getElementById("toy")
+toy.addEventListener("click", ()=>{
+    const sound_toy = document.getElementById("sound-toy")
+    sound_toy.play();
+})
+
+const dog = document.getElementById("dog")
+dog.addEventListener("click", ()=>{
+    const sound_dog = document.getElementById("sound-dog")
+    sound_dog.play();
+})
+
+const chair = document.getElementById("chair")
+chair.addEventListener("click", ()=>{
+    const sound_chair = document.getElementById("sound-chair")
+    sound_chair.play();
+})
+
+const job = document.getElementById("job")
+job.addEventListener("click", ()=>{
+    const sound_job = document.getElementById("sound-job")
+    sound_job.play();
+})
+
+const key = document.getElementById("key")
+key.addEventListener("click", ()=>{
+    const sound_key = document.getElementById("sound-key")
+    sound_key.play();
+})
+
+const gift = document.getElementById("gift")
+gift.addEventListener("click", ()=>{
+    const sound_gift = document.getElementById("sound-gift")
+    sound_gift.play();
+})
+
+document.getElementById("fun").addEventListener("click", ()=>{
+    document.getElementById("sound-fun").play();
+})
+
+document.getElementById("very").addEventListener("click", ()=>{
+    document.getElementById("sound-very").play();
+})
+
+document.getElementById("think").addEventListener("click", ()=>{
+    document.getElementById("sound-think").play();
+})
+
+document.getElementById("this").addEventListener("click", ()=>{
+    document.getElementById("sound-this").play();
+})
+
+document.getElementById("sun").addEventListener("click", ()=>{
+    document.getElementById("sound-sun").play();
+})
+
+document.getElementById("zoo").addEventListener("click", ()=>{
+    document.getElementById("sound-zoo").play();
+})
+
+document.getElementById("show").addEventListener("click", ()=>{
+    document.getElementById("sound-show").play();
+})
+
+document.getElementById("vision").addEventListener("click", ()=>{
+    document.getElementById("sound-vision").play();
+})
+
+document.getElementById("map").addEventListener("click", ()=>{
+    document.getElementById("sound-map").play();
+})
+
+document.getElementById("nice").addEventListener("click", ()=>{
+    document.getElementById("sound-nice").play();
+})
+
+document.getElementById("thing").addEventListener("click", ()=>{
+    document.getElementById("sound-thing").play();
+})
+
+document.getElementById("home").addEventListener("click", ()=>{
+    document.getElementById("sound-home").play();
+})
+
+document.getElementById("let").addEventListener("click", ()=>{
+    document.getElementById("sound-let").play();
+})
+
+document.getElementById("run").addEventListener("click", ()=>{
+    document.getElementById("sound-run").play();
+})
+
+document.getElementById("way").addEventListener("click", ()=>{
+    document.getElementById("sound-way").play();
+})
+
+document.getElementById("yes").addEventListener("click", ()=>{
+    document.getElementById("sound-yes").play();
+})
+
+document.getElementById("call").addEventListener("click", ()=>{
+    document.getElementById("sound-call").play();
+})
+
+document.getElementById("after").addEventListener("click", ()=>{
+    document.getElementById("sound-after").play();
+})
+
+document.getElementById("water").addEventListener("click", ()=>{
+    document.getElementById("sound-water").play();
+})
+
+document.getElementById("button").addEventListener("click", ()=>{
+    document.getElementById("sound-button").play();
+})
+
+document.getElementById("fire").addEventListener("click", ()=>{
+    document.getElementById("sound-fire").play();
+})
+
+document.getElementById("sport").addEventListener("click", ()=>{
+    document.getElementById("sound-sport").play();
+})
+
+document.getElementById("car").addEventListener("click", ()=>{
+    document.getElementById("sound-car").play();
+})
+
+document.getElementById("bear").addEventListener("click", ()=>{
+    document.getElementById("sound-bear").play();
+})
+
+document.getElementById("hero").addEventListener("click", ()=>{
+    document.getElementById("sound-hero").play();
+})
+
+document.getElementById("go").addEventListener("click", ()=>{
+    document.getElementById("sound-go").play();
+})
